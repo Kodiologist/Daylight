47436d6ee5a6c0ed5d2a20cc8cad11f396b87495
Kodi Arfer <git@arfer.net>
2017-05-31 12:15:42 -0700

    Try to keep Hy names even after an exception.

--- hy-lib/daylight_hy/load.py
+++ hy-lib/daylight_hy/load.py
@@ -4,40 +4,6 @@ from hy.lex import LexException
 import hy.macros
 import imp, sys, os.path
 
-def import_file_to_module(module_name, fpath, lang):
-    """Import content from fpath and put it into a Python module.
-    If module_name already exists, the preexisting module
-    is updated instead of a new module being created.
-    If a new module is created, it is installed in sys.modules.
-
-    Returns the module."""
-    preexisting = module_name in sys.modules
-    try:
-        if lang == "hy":
-            _ast = import_file_to_ast(fpath, module_name)
-        mod = (sys.modules[module_name]
-            if preexisting
-            else imp.new_module(module_name))
-        mod.__file__ = fpath
-        if lang == "hy":
-            eval(ast_compile(_ast, fpath, "exec"), mod.__dict__)
-        elif lang == "python":
-            exec(compile(open(fpath).read(), fpath, 'exec'), mod.__dict__)
-        else:
-            raise ValueError("Unknown language: {}".format(lang))
-        sys.modules[module_name] = mod
-    except (HyTypeError, LexException) as e:
-        if e.source is None:
-            with open(fpath, 'rt') as fp:
-                e.source = fp.read()
-            e.filename = fpath
-        raise
-    except Exception:
-        if not preexisting:
-            sys.modules.pop(module_name, None)
-        raise
-    return mod
-
 def repl_import(filename, code, globals_d, context_name, lang = "hy"):
     """'code' should be a Hy sexp or None. If it's None, the
     file 'filename' is opened to get the code to evaluate, and
@@ -57,22 +23,50 @@ def repl_import(filename, code, globals_d, context_name, lang = "hy"):
     - After loading two files, you can access variables defined in
       file 1 that are shadowed by variables defined in file 2."""
     mname = 'daylight-' + os.path.dirname(os.path.abspath(filename))
-    if code is None:
-        m = import_file_to_module(mname, filename, lang)
-        returnval = None
-    else:
-        if lang != 'hy':
-            raise NotImplementedError
-        m = sys.modules.get(mname)
-        if not m:
-            m = imp.new_module(mname)
-            sys.modules[mname] = m
-        returnval = hy_eval(code, m.__dict__, mname)
-    # Copy the module's names (except for names starting with "_")
-    # into our globals.
-    for k in dir(m):
-       if not k.startswith('_'):
-           globals_d[k] = getattr(m, k)
-    # Import macros, too.
+    m = None
+    try:
+        if code is None:
+            preexisting = mname in sys.modules
+            try:
+                if lang == "hy":
+                    _ast = import_file_to_ast(filename, mname)
+                m = (sys.modules[mname]
+                    if preexisting
+                    else imp.new_module(mname))
+                m.__file__ = filename
+                if lang == "hy":
+                    eval(ast_compile(_ast, filename, "exec"), m.__dict__)
+                elif lang == "python":
+                    exec(compile(open(filename).read(), filename, 'exec'), m.__dict__)
+                else:
+                    raise ValueError("Unknown language: {}".format(lang))
+                sys.modules[mname] = m
+            except (HyTypeError, LexException) as e:
+                if e.source is None:
+                    with open(filename, 'rt') as fp:
+                        e.source = fp.read()
+                    e.filename = filename
+                raise
+            except Exception:
+                if not preexisting:
+                    sys.modules.pop(mname, None)
+                raise
+            returnval = None
+        else:
+            if lang != 'hy':
+                raise NotImplementedError
+            m = sys.modules.get(mname)
+            if not m:
+                m = imp.new_module(mname)
+                sys.modules[mname] = m
+            returnval = hy_eval(code, m.__dict__, mname)
+    finally:
+        if m is not None:
+            # Copy the module's names (except for names starting with "_")
+            # into our globals.
+            for k in dir(m):
+               if not k.startswith('_'):
+                   globals_d[k] = getattr(m, k)
+    # Import macros.
     hy.macros.require(mname, context_name, all_macros = True)
     return returnval
